fdae7bd227ef30b40ac0bd706966b1e742ba4495
Ilshat Galimov <einnks@gmail.com>
2015-10-30 19:59:00 +0300

    Some refactoring, added new methods and exceptions handling

--- Lab2/Car.cs
+++ Lab2/Car.cs
@@ -4,94 +4,38 @@ namespace Lab2
 {
     class Car
     {
-        protected string manufacturer;
-        protected int cylinderCount;
-        protected int power;
+        public string Manufacturer { get; set; }
+        public int CylinderCount { get; set; }
+        public int Power { get; set; }
 
         public Car()
         {
-            manufacturer = "N/A";
-            cylinderCount = 0;
-            power = 0;
-        }
-
-        public string Manufacturer
-        {
-            get { return manufacturer; }
-            set
-            {
-                try { manufacturer = value; }
-                catch (FormatException exception)
-                {
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine("Введите марку заново.");
-                }
-            }
-        }
-
-        public int CylinderCount
-        {
-            get { return cylinderCount; }
-            set
-            {
-                try { cylinderCount = value; }
-                catch (FormatException exception)
-                {
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine("Введите количество цилиндров двигателя заново.");
-                }
-            }
-        }
-
-        public int Power
-        {
-            get { return power; }
-            set
-            {
-                try { power = value; }
-                catch (FormatException exception)
-                {
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine("Введите мощность двигателя заново.");
-                }
-            }
+            Manufacturer = "N/A";
+            CylinderCount = 0;
+            Power = 0;
         }
 
         public virtual void Print()
         {
-            Console.WriteLine("Марка автомобиля: {0}", manufacturer);
-            Console.WriteLine("Количество цилиндров двигателя: {0}", cylinderCount);
-            Console.WriteLine("Мощность двигателя: {0}", power);
+            Console.WriteLine("Марка автомобиля: {0}", Manufacturer);
+            Console.WriteLine("Количество цилиндров двигателя: {0}", CylinderCount);
+            Console.WriteLine("Мощность двигателя: {0}", Power);
         }
     }
 
     class Lorry : Car
     {
-        protected int carrying;
+        public int Carrying { get; set; }
 
         public Lorry()
         {
-            carrying = 0;
-        }
-
-        public int Carrying
-        {
-            get { return carrying; }
-            set
-            {
-                try { carrying = value; }
-                catch (FormatException exception)
-                {
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine("Введите грузоподъемность заново.");
-                }
-            }
+            Carrying = 0;
         }
 
         public override void Print()
         {
             base.Print();
-            Console.WriteLine("Грузоподъёмность: {0}", carrying);
+            Console.WriteLine("Грузоподъёмность: {0}", Carrying);
         }
 
     }
@@ -113,28 +57,185 @@ namespace Lab2
                 {
                     case "1":
                         Console.Clear();
-                        string userInput1;
-                        do
-                        {
-                            userInput1 = DisplayCarMenu();
-
-                            switch (userInput1)
-                            {
-                                case "1":
-                                    Console.Clear();
-                                    break;
-                                default:
-                                    Console.Clear();
-                                    break;
-                            }
-                        } while (userInput1 != "5");
+                        CarMenuLogic(car);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        LorryMenuLogic(lorry);
                         break;
                     default:
                         Console.Clear();
-                        Console.WriteLine("ERROR: unexpected menu number!");
+                        Console.WriteLine("Неверный пункт меню.");
                         break;
                 }
             } while (userInput != "3");
+        }
+
+        private static void SetManufacturer(Car car)
+        {
+            try
+            {
+                car.Manufacturer = Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите марку автомобиля заново");
+            }
+        }
+
+        private static void SetManufacturer(Lorry lorry)
+        {
+            try
+            {
+                lorry.Manufacturer = Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите марку грузовика заново.");
+            }
+        }
+
+        private static void SetCylinderCount(Car car)
+        {
+            try
+            {
+                car.CylinderCount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите количество цилиндров двигателя заново.");
+            }
+        }
+
+        private static void SetCylinderCount(Lorry lorry)
+        {
+            try
+            {
+                lorry.CylinderCount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите количество цилиндров двигателя заново.");
+            }
+        }
+
+        private static void SetPower(Car car)
+        {
+            try
+            {
+                car.Power = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите мощность двигателя заново.");
+            }
+        }
+
+        private static void SetPower(Lorry lorry)
+        {
+            try
+            {
+                lorry.Power = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите мощность двигателя заново.");
+            }
+        }
+
+        private static void SetCarrying(Lorry lorry)
+        {
+            try
+            {
+                lorry.Carrying = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Введите грузоподъёмность заново.");
+            }
+        }
+
+        private static void CarMenuLogic(Car car)
+        {
+            string userInput;
+            do
+            {
+                userInput = DisplayCarMenu();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        SetManufacturer(car);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        SetCylinderCount(car);
+                        break;
+                    case "3":
+                        Console.Clear();
+                        SetPower(car);
+                        break;
+                    case "4":
+                        Console.Clear();
+                        car.Print();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный пункт меню.");
+                        break;
+                }
+            } while (userInput != "5");
+        }
+
+        private static void LorryMenuLogic(Lorry lorry)
+        {
+            string userInput;
+            do
+            {
+                userInput = DisplayLorryMenu();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        SetManufacturer(lorry);
+                        break;
+                    case "2":
+                        Console.Clear();
+                        SetCylinderCount(lorry);
+                        break;
+                    case "3":
+                        Console.Clear();
+                        SetPower(lorry);
+                        break;
+                    case "4":
+                        Console.Clear();
+                        SetCarrying(lorry);
+                        break;
+                    case "5":
+                        Console.Clear();
+                        lorry.Print();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Неверный пункт меню.");
+                        break;
+                }
+            } while (userInput != "6");
         }
 
         private static string DisplayMenu()
@@ -164,8 +265,8 @@ namespace Lab2
             Console.WriteLine("1. Ввод марки.");
             Console.WriteLine("2. Ввод количества цилиндров.");
             Console.WriteLine("3. Ввод мощности двигателя.");
-            Console.WriteLine("4. Вывод показателей авто.");
-            Console.WriteLine("5. Ввод грузоподъёмности.");
+            Console.WriteLine("4. Ввод грузоподъёмности.");
+            Console.WriteLine("5. Вывод показателей грузовика.");
             Console.WriteLine("6. Возврат к предыдущему меню.");
 
             var cki = Console.ReadKey(false);
